d4ad6e4fe94069d0a0ba22460e2f44fe3c48779e
willdu <willdu@vipabc.com>
2018-10-25 15:36:46 +0800

    test

---
+++ src/main/java/me/will/jackson/model/GetMemberMappingResult.java
@@ -0,0 +1,36 @@
+package me.will.jackson.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class GetMemberMappingResult extends UpperResult {
+    @JsonProperty("Data")
+    @JsonDeserialize(as= ArrayList.class, contentAs= Model.class)
+    private List<Model> items;
+
+    @Data
+    public static class Model{
+        @JsonProperty("OpenId")
+        private String openId;
+        @JsonProperty("UnionId")
+        private String unionId;
+        @JsonProperty("ClientSn")
+        private Integer clientSn;
+        @JsonProperty("LeadSn")
+        private Integer leadSn;
+        @JsonProperty("ClientName")
+        private String clientName;
+        @JsonProperty("BrandId")
+        private Integer brandId;
+        @JsonProperty("VipJr")
+        private Integer vipJr;
+    }
+
+}
---
+++ src/main/java/me/will/jackson/model/UpperResult.java
@@ -0,0 +1,29 @@
+package me.will.jackson.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+/**
+ * json字段首字母是大写的
+ */
+@Data
+public class UpperResult {
+
+    @JsonProperty("Code")
+    private String code;
+
+    @JsonProperty("Msg")
+    private String msg;
+
+    @JsonProperty("ErrMsg")
+    private String errMsg;
+
+    @JsonProperty("IsSuccess")
+    private Boolean isSuccess;
+
+    @JsonProperty("SubCode")
+    private String subCode;
+
+    @JsonProperty("SubMsg")
+    private String subMsg;
+}
